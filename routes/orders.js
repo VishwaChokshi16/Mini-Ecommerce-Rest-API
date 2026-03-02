cd017a425eba44099b0a060f9e1e73bc977278cc
Vishwa Chokshi <vishwachokshi1605@gmail.com>
2026-03-02 21:56:51 +0530

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,27 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const validate = require('../middleware/validate');
+const orderController = require('../controllers/orderController');
 
-let orders = [];
-
-// GET all orders
-router.get('/', (req, res) => {
-  res.json(orders);
-});
-
-// PLACE order
-router.post('/', validate(['userId', 'totalAmount']), (req, res) => {
-
-  const newOrder = {
-    id: orders.length + 1,
-    userId: req.body.userId,
-    totalAmount: req.body.totalAmount,
-    date: new Date()
-  };
-
-  orders.push(newOrder);
-
-  res.status(201).json(newOrder);
-});
+router.get('/', orderController.getOrders);
+router.post('/', validate(['userId', 'totalAmount']), orderController.createOrder);
 
 module.exports = router;
+
